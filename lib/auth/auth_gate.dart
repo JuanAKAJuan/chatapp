4eb5f52706794479292f1ed0f56b08b5bc963ede
Juan <juancnmireles@gmail.com>
2024-03-06 20:43:46 -0500

    Setup firebase, fix pixel overflow issue, and begin implementing a drawer

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,26 @@
+import 'package:chatapp/auth/login_or_register.dart';
+import 'package:chatapp/pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // User is logged in
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            // User is NOT logged in
+            return const LoginOrRegister();
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,120 @@
+import 'package:chatapp/auth/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:chatapp/components/my_textfield.dart';
+import 'package:chatapp/components/my_button.dart';
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  final void Function()? onTap;
+
+  RegisterPage({super.key, required this.onTap});
+
+  void register(BuildContext context) {
+    final _auth = AuthService();
+
+    // Passwords match -> create user
+    if (_passwordController.text == _confirmPasswordController.text) {
+      try {
+        _auth.signUpWithEmailPassword(
+          _emailController.text,
+          _passwordController.text,
+        );
+      } catch (e) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(e.toString()),
+          ),
+        );
+      }
+    }
+
+    // Passwords don't match -> tell user to fix their password
+    else {
+      showDialog(
+          context: context,
+          builder: (context) => const AlertDialog(
+            title: Text("Passwords don't match!"),
+            ),
+          );
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.message,
+                size: 60,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              const SizedBox(height: 40),
+              Text(
+                "Let's create an account for you",
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 25),
+              MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: _emailController,
+              ),
+              const SizedBox(height: 10),
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: _passwordController,
+              ),
+              const SizedBox(height: 10),
+              MyTextField(
+                hintText: "Confirm password",
+                obscureText: true,
+                controller: _confirmPasswordController,
+              ),
+              const SizedBox(height: 25),
+              MyButton(
+                text: "Register",
+                onTap: () => register(context),
+              ),
+              const SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account? ",
+                    style:
+                        TextStyle(color: Theme.of(context).colorScheme.primary),
+                  ),
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Text(
+                      "Login now",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
